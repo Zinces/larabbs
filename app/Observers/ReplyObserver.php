0a98d71d10448131613bd999219c0c15f4314ca2
zinces <305977606@qq.com>
2019-02-27 08:23:31 +0000

    2019-2-27 16:23

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -11,11 +12,19 @@ class ReplyObserver
 {
     public function creating(Reply $reply)
     {
-        //
+        $reply->content = clean($reply->content, 'user_topic_body');
     }
 
-    public function updating(Reply $reply)
+    public function created(Reply $reply)
     {
-        //
+        $reply->topic->increment('reply_count', 1);
+
+        // 通知作者话题被回复了
+        $reply->topic->user->notify(new TopicReplied($reply));
+    }
+
+    public function deleted(Reply $reply)
+    {
+        $reply->topic->decrement('reply_count', 1);
     }
 }
--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -64,8 +64,8 @@
             {{-- 用户回复列表 --}}
             <div class="panel panel-default topic-reply">
                 <div class="panel-body">
-                    @include('topics._reply_box', ['topic' => $topic])
-                    @include('topics._reply_list', ['replies' => $topic->replies()->with('user')->get()])
+                    @includeWhen(Auth::check(),'topics._reply_box', ['topic' => $topic])
+                    @include('topics._reply_list', ['replies' => $topic->replies()->with('user','topic')->latest()->get()])
                 </div>
             </div>
         </div>
